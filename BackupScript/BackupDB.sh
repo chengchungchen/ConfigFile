7712f46d80a8b4c5023b3e183682c34726e0170d
Cheng-Chung Chen <chengchungchen@talk2yam.com>
2015-06-01 17:52:43 +0800

    Editing the backup scripts

--- BackupScript/BackupDB.sh
+++ BackupScript/BackupDB.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
-# This script is to backup the DB
-
-DATE=$(date +%Y%m%d-%H%M%S)
-BACKUP_PATH='/home/log/backup/DB'
-BACKUP_FILE=$BACKUP_PATH/ccchen_DB$DATE.sql.bz2
-
-if [ ! -d $BACKUP_PATH ]; then
-	mkdir -p $BACKUP_PATH
-fi
-
-starttime=$(date +%H%M%S)
-echo $starttime
-/usr/bin/mysqldump --default-character-set=utf8 --set-gtid-purged=OFF --opt --quick --all-databases --triggers --routines --events | /bin/bzip2 > $BACKUP_FILE 
-endtime=$(date +H%M%S)
-echo $endtime
+# This script is to backup the each DB and user privileges
 
 function rm_oldfile() {
-	file=$1
-	n=$2
-	n1=$(ls -1 $file* | wc -l)
-	n2=$(ls -1 $file* | tail -$n | wc -l)
-	n3=$(expr $n1 - $n2)
+    file=$1
+    n=$2
+    n1=$(ls -1 $file* | wc -l)
+    n2=$(ls -1 $file* | tail -$n | wc -l)
+    n3=$(expr $n1 - $n2)
 
-	ls -1 $file* | head -n $n3 | xargs -n 1 rm -f
+    ls -1 $file* | head -n $n3 | xargs -n 1 rm -f
 }
+
+DATE=$(date +%Y%m%d-%H%M%S)
+
+# Read the DB list, and backup it.
+while read line
+do
+    DBNAME="$line"
+    BACKUP_PATH="/home/log/backup/DB/$DBNAME"
+    BACKUP_FILE="$BACKUP_PATH/ccchen_$DBNAME$DATE.sql.bz2"
+
+    if [ ! -d $BACKUP_PATH ]; then
+        mkdir -p $BACKUP_PATH
+    fi
+
+    /usr/bin/mysqldump --default-character-set=utf8 --set-gtid-purged=OFF --opt --quick --triggers --routines --events $DBNAME | /bin/bzip2 > $BACKUP_FILE
+
+    rm_oldfile $BACKUP_PATH/ 5
+done < DBList.txt
+
+# Backup DB user privileges
+if [ ! -d /home/log/backup/DB/user ]; then
+    mkdir -p /home/log/backup/DB/user
+fi
+
+/usr/bin/mysqldump --default-character-set=utf8 mysql columns_priv db procs_priv tables_priv user | /usr/bin/bzip2 > /home/log/backup/DB/user/ccchen_dbuser$DATE.sql.bz2
 
 rm_oldfile $BACKUP_PATH/ 5
--- BackupScript/BackupWEB.sh
+++ BackupScript/BackupWEB.sh
@@ -2,26 +2,31 @@
 # This script is to backup the web data
 # /root/BackupScript/WEBList.txt
 
-WEB_DIR='/home/www/ccchen.com'
+#WEB_DIR='/home/www/ccchen.com'
+WEB_DIR=""
+while read line
+do
+    WEB_DIR="$WEB_DIR $line"
+done < WEBList.txt
 
 DATE=$(date +%Y%m%d-%H%M%S)
 BACKUP_PATH='/home/log/backup/WEB'
 BACKUP_FILE=$BACKUP_PATH/ccchen_WEB$DATE.tgz
 
 if [ ! -d $BACKUP_PATH ]; then
-	mkdir -p $BACKUP_PATH
+    mkdir -p $BACKUP_PATH
 fi
 
 /bin/tar zcvf $BACKUP_FILE $WEB_DIR > /dev/null 2>&1
 
 function rm_oldfile() {
-	file=$1
-	n=$2
-	n1=$(ls -1 $file* | wc -l)
-	n2=$(ls -1 $file* | tail -$n | wc -l)
-	n3=$(expr $n1 - $n2)
+    file=$1
+    n=$2
+    n1=$(ls -1 $file* | wc -l)
+    n2=$(ls -1 $file* | tail -$n | wc -l)
+    n3=$(expr $n1 - $n2)
 
-	ls -1 $file* | head -n $n3 | xargs -n 1 rm -f
+    ls -1 $file* | head -n $n3 | xargs -n 1 rm -f
 }
 
 rm_oldfile $BACKUP_PATH/ 7
